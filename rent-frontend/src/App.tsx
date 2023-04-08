f303f3f5b4fca1f4a71d86663d1ebad28e5b2244
Murat <murat_a@mail.ru>
2023-04-08 13:03:41 +0600

    some template

--- rent-frontend/src/App.tsx
+++ rent-frontend/src/App.tsx
@@ -1,25 +1,109 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+    Typography,
+    AppBar,
+    Card,
+    CardActions,
+    CardContent,
+    CardMedia,
+    CssBaseline,
+    Grid,
+    Toolbar,
+    Container, Button
+} from '@mui/material'
+import { PhotoCamera } from '@mui/icons-material'
+
+const cards = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <CssBaseline />
+        <AppBar position="relative">
+            <Toolbar>
+                <PhotoCamera/>
+                <Typography variant="h6">
+                    Главная страница
+                </Typography>
+            </Toolbar>
+        </AppBar>
+        <main>
+            <div>
+                <Container maxWidth="sm">
+                    <Typography variant="h2"
+                                align="center"
+                                color="textPrimary"
+                                gutterBottom
+                    >
+                        Главная страница
+                    </Typography>
+                    <Typography variant="h5"
+                                align="center"
+                                color="textSecondary"
+                                paragraph>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi, incidunt?
+                    </Typography>
+                    <div>
+                        <Grid container spacing={2} justifyContent="center" columns={{xs: 6, md: 6}}>
+                            <Grid item>
+                                <Button variant="contained" color="primary">
+                                    Посмотреть журнал
+                                </Button>
+                            </Grid>
+                            <Grid item>
+                                <Button variant="outlined" color="primary">
+                                    Добавить новую квартиру
+                                </Button>
+                            </Grid>
+                        </Grid>
+                    </div>
+                </Container>
+            </div>
+            <Container maxWidth="md"
+                       className={'cardGrid'}
+            >
+                <Grid container spacing={4}>
+                    {cards.map((card) => (
+                        <Grid item key={card} xs={12} sm={6} md={4}>
+                            <Card
+                                className={'card'}
+                            >
+                                <CardMedia
+                                    className={'cardMedia'}
+                                    image="https://source.unsplash.com/random"
+                                    title="Image title">
+                                </CardMedia>
+                                <CardContent
+                                    className={'cardContent'}
+                                >
+                                    <Typography gutterBottom variant="h5">
+                                        Heading
+                                    </Typography>
+                                    <Typography>
+                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aliquam atque beatae deserunt eaque et illum magni nemo numquam quia!
+                                    </Typography>
+                                </CardContent>
+                                <CardActions>
+                                    <Button size="small" color="primary">Открыть</Button>
+                                    <Button size="small" color="primary">Редактировать</Button>
+                                </CardActions>
+                            </Card>
+                        </Grid>
+                    ))}
+                </Grid>
+            </Container>
+
+        </main>
+        <footer className={'footer'}>
+            <Typography variant='h6' align='center' gutterBottom>
+                Footer
+            </Typography>
+            <Typography variant='subtitle1' align='center' color='textSecondary'>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur dolorem expedita fugit ipsa, ipsum labore molestiae molestias odio perferendis voluptatum.
+            </Typography>
+        </footer>
+    </>
   );
 }
 
